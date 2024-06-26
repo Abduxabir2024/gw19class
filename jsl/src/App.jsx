993aa90172a57c514c92ffc19492a6ba1ca50a45
Abduxabir2024 <abduqayumovabduxabir2@gmail.com>
2024-06-26 18:47:16 +0500

    olin

--- jsl/src/App.jsx
+++ jsl/src/App.jsx
@@ -1,11 +1,14 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
     <>
-    
+    <Routes>
+      <Route path='/' element={<h1>Lorem ipsum dolor sit.</h1>}/>
+    </Routes>
     </>
   )
 }
