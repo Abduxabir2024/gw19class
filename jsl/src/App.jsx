450cc96f67417f2cbd5f434046451507cda8e492
Abduxabir2024 <abduqayumovabduxabir2@gmail.com>
2024-06-26 19:06:58 +0500

    so

--- jsl/src/App.jsx
+++ jsl/src/App.jsx
@@ -1,14 +1,22 @@
 
-import { Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Page1 from './page/page1'
+import Page2 from './page/page2'
+import Page3 from './page/page3'
 
 function App() {
 
   return (
     <>
+    <Link to={"/"}>Page1</Link>
+    <Link to={"/page2"}>Page2</Link>
+    <Link to={"/page3"}>Page3</Link>
     <Routes>
-      <Route path='/' element={<h1>Lorem ipsum dolor sit.</h1>}/>
-    </Routes>
+      <Route path='/' element={<Page1/>}/>
+      <Route path='/page2' element={<Page2/>}/>
+      <Route path='/page3' element={<Page3/>}/>
+      </Routes>
     </>
   )
 }
